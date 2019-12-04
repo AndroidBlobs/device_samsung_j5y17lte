1035cbbc2ec2fcb952d4023453709845c66fa145
ShivamKumarJha <jha.shivam3@gmail.com>
2019-12-04 06:04:36 +0000

    j5y17ltexx-user 8.1.0 M1AJQ J530FXXU5BSG1 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/samsung/j5y17lte
+BOARD_VENDOR := samsung
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/samsung/j5y17lte/BoardConfigVendor.mk
---
+++ lineage_j5y17lte.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from j5y17lte device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := j5y17lte
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_NAME := lineage_j5y17lte
+PRODUCT_MODEL := SM-J530F
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+TARGET_VENDOR := samsung
+TARGET_VENDOR_PRODUCT_NAME := j5y17lte
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="j5y17ltexx-user 8.1.0 M1AJQ J530FXXU5BSG1 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := samsung/j5y17ltexx/j5y17lte:8.1.0/M1AJQ/J530FXXU5BSG1:user/release-keys
